a9664a90a5e0628d57fe73f02f0da55dedd54a16
hoopyfreud <7440953+HoopyFreud@users.noreply.github.com>
2026-05-29 17:39:32 -0400

    starting selector development

--- src/App.svelte
+++ src/App.svelte
@@ -1,161 +1,74 @@
 <script>
 	import './app.css';
-	import svelteLogo from './assets/svelte.svg';
-	import viteLogo from './assets/vite.svg';
-	import heroImg from './assets/hero.png';
-	import Counter from './lib/Counter.svelte';
-  import { Button } from "$lib/components/ui/button/index.js";
+  	import * as Command from "$lib/components/ui/command-scrollbar/index.js";
+    import Button from '$lib/components/ui/button/button.svelte';
+
+	const cardGroups = [
+	{
+		value: "coins",
+		label: "Coins",
+		cards: [
+		{
+			value: "1C",
+			label: "Ace of Coins"
+		},
+		{
+			value: "2C",
+			label: "2 of Coins"
+		},
+		{
+			value: "3C",
+			label: "3 of Coins"
+		}
+		]
+	},
+	{
+		value: "wands",
+		label: "Wands",
+		cards: [
+		{
+			value: "1W",
+			label: "Ace of Wands"
+		},
+		{
+			value: "2W",
+			label: "2 of Wands"
+		},
+		{
+			value: "3W",
+			label: "3 of Wands"
+		}
+		]
+	}
+  ];
+
+  const flatCards = cardGroups.flatMap((suit) => suit.cards);
+
+  let selectValue = $state("");
+  let inputLabel = $derived(flatCards.find((f) => f.value == selectValue)?.label);
+  let selectLabel = $derived(flatCards.find((f) => f.value == selectValue)?.label);
 </script>
+ 
+<Command.Root class="rounded-lg border shadow-md md:min-w-[450px]" value={selectValue}>
+ <Command.Input placeholder="Select or search for a card" bind:value={inputLabel}/>
+ <Command.List class="overflow-y-auto h-40">
+  <Command.Empty>No cards found</Command.Empty>
+  {#each cardGroups as suit}
+  	<Command.Group heading={suit.label}>
+	  {#each suit.cards as card}
+   		<Command.Item aria-selected={card.value === selectValue?true:false}
+		onSelect={() => {
+		  selectValue = card.value;
+		}}>
+    	  <span>{card.label}</span>
+   		</Command.Item>
+	  {/each}
+  	</Command.Group>
+    <Command.Separator />
+  {/each}
+ </Command.List>
+</Command.Root>
 
-<section id="center">
-	<div class="hero">
-		<img
-			src={heroImg}
-			class="base"
-			width="170"
-			height="179"
-			alt=""
-		/>
+<Button variant="outline">Add to Tableau</Button>
 
-		<img
-			src={svelteLogo}
-			class="framework"
-			alt="Svelte logo"
-		/>
-
-		<img src={viteLogo} class="vite" alt="Vite logo" />
-	</div>
-
-	<div>
-		<h1>Get started</h1>
-
-		<p>
-			Edit 
-			<code>src/App.svelte</code>
-			and save to test 
-			<code>HMR</code>
-		</p>
-	</div>
-
-	<Counter />
-  <Button>Click me</Button>
-</section>
-
-<div class="ticks"></div>
-
-<section id="next-steps">
-	<div id="docs">
-		<svg
-			class="icon"
-			role="presentation"
-			aria-hidden="true"
-		><use href="/icons.svg#documentation-icon"></use></svg>
-
-		<h2>Documentation</h2>
-		<p>Your questions, answered</p>
-
-		<ul>
-			<li>
-				<a
-					href="https://vite.dev/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<img class="logo" src={viteLogo} alt="" />
-					Explore Vite
-				</a>
-			</li>
-
-			<li>
-				<a
-					href="https://svelte.dev/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<img class="button-icon" src={svelteLogo} alt="" />
-					Learn more
-				</a>
-			</li>
-		</ul>
-	</div>
-
-	<div id="social">
-		<svg
-			class="icon"
-			role="presentation"
-			aria-hidden="true"
-		><use href="/icons.svg#social-icon"></use></svg>
-
-		<h2>Connect with us</h2>
-		<p>Join the Vite community</p>
-
-		<ul>
-			<li>
-				<a
-					href="https://github.com/vitejs/vite"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<svg
-						class="button-icon"
-						role="presentation"
-						aria-hidden="true"
-					><use href="/icons.svg#github-icon"></use></svg>
-
-					GitHub
-				</a>
-			</li>
-
-			<li>
-				<a
-					href="https://chat.vite.dev/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<svg
-						class="button-icon"
-						role="presentation"
-						aria-hidden="true"
-					><use href="/icons.svg#discord-icon"></use></svg>
-
-					Discord
-				</a>
-			</li>
-
-			<li>
-				<a
-					href="https://x.com/vite_js"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<svg
-						class="button-icon"
-						role="presentation"
-						aria-hidden="true"
-					><use href="/icons.svg#x-icon"></use></svg>
-
-					X.com
-				</a>
-			</li>
-
-			<li>
-				<a
-					href="https://bsky.app/profile/vite.dev"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<svg
-						class="button-icon"
-						role="presentation"
-						aria-hidden="true"
-					><use href="/icons.svg#bluesky-icon"></use></svg>
-
-					Bluesky
-				</a>
-			</li>
-		</ul>
-	</div>
-</section>
-
-<div class="ticks"></div>
-<section id="spacer"></section>
+<span>{selectLabel}</span>
---
+++ src/lib/components/ui/command-scrollbar/index.ts
@@ -0,0 +1,37 @@
+import Root from "./command-scrollbar.svelte";
+import Loading from "./command-scrollbar-loading.svelte";
+import Dialog from "./command-scrollbar-dialog.svelte";
+import Empty from "./command-scrollbar-empty.svelte";
+import Group from "./command-scrollbar-group.svelte";
+import Item from "./command-scrollbar-item.svelte";
+import Input from "./command-scrollbar-input.svelte";
+import List from "./command-scrollbar-list.svelte";
+import Separator from "./command-scrollbar-separator.svelte";
+import Shortcut from "./command-scrollbar-shortcut.svelte";
+import LinkItem from "./command-scrollbar-link-item.svelte";
+
+export {
+	Root,
+	Dialog,
+	Empty,
+	Group,
+	Item,
+	LinkItem,
+	Input,
+	List,
+	Separator,
+	Shortcut,
+	Loading,
+	//
+	Root as Command,
+	Dialog as CommandDialog,
+	Empty as CommandEmpty,
+	Group as CommandGroup,
+	Item as CommandItem,
+	LinkItem as CommandLinkItem,
+	Input as CommandInput,
+	List as CommandList,
+	Separator as CommandSeparator,
+	Shortcut as CommandShortcut,
+	Loading as CommandLoading,
+};
