9b6129fa759b645557ad1a189ea4f7ad3ab8f198
hoopyfreud <7440953+HoopyFreud@users.noreply.github.com>
2026-08-06 19:47:15 -0400

    added capacitor config file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mechanist.tlookup',
+  appName: 'Tarot',
+  webDir: 'dist',
+};
+
+export default config;
