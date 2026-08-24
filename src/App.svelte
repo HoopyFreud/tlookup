5331911d71af3e6a498d25e78ab6780a2d37d51e
hoopyfreud <7440953+HoopyFreud@users.noreply.github.com>
2026-08-23 21:39:47 -0400

    filename case fix

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
 
 	import { Buffer } from "buffer"
 
-	import cardTableB64 from "./lib/cardTableb64.raw?raw";
+	import cardTableB64 from "./lib/cardTableB64?raw";
 
 	const cardTable = JSON.parse(Buffer.from(cardTableB64, "base64").toString())
 
